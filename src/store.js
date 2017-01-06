4731cccb70405bcb1ca46e1e5dea27db488d2abe
Rob Huehn <rhuehn@gmail.com>
2017-01-06 12:08:46 -0800

    add remote-redux-devtools

--- src/store.js
+++ src/store.js
@@ -1,5 +1,10 @@
 import bus, {EventEmitter} from '@theatersoft/bus'
 import {combineReducers, createStore} from 'redux'
+import devToolsEnhancer from 'remote-redux-devtools'
 import reducer from './reducer'
 
-export default createStore(reducer, {devices: []})
+export default createStore(
+    reducer,
+    {devices: []},
+    devToolsEnhancer({realtime: true, port: 6400})
+)
