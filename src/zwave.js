2127fe64a7a069a509107e3083fd3d0c23f92e2c
Rob Huehn <rhuehn@gmail.com>
2017-01-19 13:18:15 -0800

    test with SaveConfiguration set

--- src/zwave.js
+++ src/zwave.js
@@ -7,19 +7,19 @@ import {nodeSet, readyNode, addValue, changeValue, valueRemoved} from './actions
 const zwave = new OpenZwave({
     Logging: true,
     ConsoleOutput: true,
-    SaveConfiguration: false
+    SaveConfiguration: true
 })
 export default zwave
-export function setStore (store) {
+export function setStore ({dispatch}) {
     zwave
         .on('connected', v => log('connected', v))
         .on('driver ready', hid => log(`driver ready`, hid.toString(16)))
         .on('driver failed', () => (log('driver failed'), zwave.disconnect()))
-        .on('node added', nid => store.dispatch(nodeSet(nid)))
-        .on('value added', (nid, cid, value) => store.dispatch(addValue(value)))
-        .on('value changed', (nid, cid, value) => store.dispatch(changeValue(value)))
-        .on('value removed', (nid, cid, index) => store.dispatch(valueRemoved(nid, cid, index)))
-        .on('node ready', (nid, nodeinfo) => store.dispatch(readyNode(nid, nodeinfo)))
+        .on('node added', nid => dispatch(nodeSet(nid)))
+        .on('value added', (nid, cid, value) => dispatch(addValue(value)))
+        .on('value changed', (nid, cid, value) => dispatch(changeValue(value)))
+        .on('value removed', (nid, cid, index) => dispatch(valueRemoved(nid, cid, index)))
+        .on('node ready', (nid, nodeinfo) => dispatch(readyNode(nid, nodeinfo)))
         .on('notification', (nid, notif) => log('notification', nid, notif))
         .on('scan complete', () => log('scan complete'))
         .on('controller command', (r, s) => log('controller commmand feedback', r, s))
