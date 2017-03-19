cfc0e36907cd09edb32192183ce05266c7cfb425
Rob Huehn <rhuehn@gmail.com>
2017-03-19 15:12:08 -0700

    get values value by precomputed or cached cid

--- src/actions/apiActions.js
+++ src/actions/apiActions.js
@@ -51,11 +51,11 @@ export const
         dispatch(valueSet(value))
         const
             id = String(value.node_id),
-            device = getState().devices[id]
+            {devices: {[id]: device}, nodes: {[id]: node}} = getState()
         if (!device) return
         const
             intf = interfaceOfType(device.type),
-            cid = cidOfInterface(intf),
+            cid = node.cid || cidOfInterface(intf), /// ???
             index = getCidValueIndex(cid),
             deviceValue = normalizeInterfaceValue(intf, value.value)
         if (cid === value.class_id && index === value.index && device.value !== deviceValue)
@@ -71,7 +71,7 @@ export const
         if (device) {
             dispatch(deviceSet(device))
             if (needsPoll(info.cid))
-                zwave.enablePoll(nid, cid)
+                zwave.enablePoll(nid, info.cid)
         }
     }
 
@@ -85,11 +85,13 @@ const
         if (type === Type.SecuritySensor && product && product.includes('Motion'))
             type = Type.MotionSensor
         if (type) {
-            const id = String(nid);
-            let cid = cidMap(nodeinfo);
-            ({name, type, cid} = update({id, name, type, cid}))
+            const
+                id = String(nid),
+                intf = interfaceOfType(type),
+                cid = cidMap(nodeinfo) || cidOfInterface(intf);
+            ({name, type} = update({id, name, type, cid}))
             if (!name) name = `ZWave.${id}`
-            const value = normalizeInterfaceValue(interfaceOfType(type), getTypeValuesValue(type, values))
+            const value = normalizeInterfaceValue(intf, getCidValuesValue(cid, values))
             device = {name, value, type, id}
             nodeinfo = {...nodeinfo, cid}
         }
@@ -102,7 +104,7 @@ const
             [CommandClass.Alarm, Type.SecuritySensor]
         ])
         for (const [cid, type] of map.entries())
-            if (getCidValuesValue(cid, values)) return type
+            if (getCidValuesValue(cid, values) !== undefined) return type
     },
     cidOfInterface = intf => ({
         [Interface.SWITCH_BINARY]: CommandClass.BinarySwitch,
@@ -110,14 +112,8 @@ const
         [Interface.SENSOR_BINARY]: CommandClass.Alarm
     }[intf]),
     getCidValuesValue = (cid, values) => {
-        const entry = Object.entries(values)
-            .find(([k, v]) => v.class_id === cid && v.index === getCidValueIndex(cid))
-        if (entry) return entry[1]
-    },
-    getInterfaceValuesValue = (intf, values) => {
-        const
-            cid = cidOfInterface(intf),
-            value = getCidValuesValue(cid, values)
+        const value = Object.values(values)
+            .find(v => v.class_id === cid && v.index === getCidValueIndex(cid))
         return value && value.value
     },
     normalizeInterfaceValue = (intf, value) => {
@@ -128,8 +124,6 @@ const
         }
         return value
     },
-    getTypeValuesValue = (type, values) =>
-        getInterfaceValuesValue(interfaceOfType(type), values),
     getCidValueIndex = cid =>
         cid === CommandClass.Alarm ? 1 : 0,
     needsPoll = cid =>
