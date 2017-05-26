9926d0551861e868a3baa255ee07e735a00e13f9
Rob Huehn <rhuehn@gmail.com>
2017-05-25 18:07:39 -0700

    fix dedup prev state default value

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const equal = (a, b, _a = Object.keys(a), _b = Object.keys(b)) => (
     _a.length === _b.length && !_a.find(k => !_b.includes(k) || a[k] !== b[k])
 )
 
-const dedup = (getState, _state = getState()) => f => (_next = getState()) => {
+const dedup = (getState, _state = {}) => f => (_next = getState()) => {
     if (!equal(_next, _state)) {
         _state = _next
         f(_next)
