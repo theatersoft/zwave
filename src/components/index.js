a6a0d890c12a075b4c998a70e2b7127e3d5a1d88
Rob Huehn <rhuehn@gmail.com>
2017-07-23 11:07:25 -0700

    impl add/remove service settings

--- src/components/index.js
+++ src/components/index.js
@@ -3,26 +3,53 @@ import {ListItem, Switch} from '@theatersoft/components'
 import {proxy} from '@theatersoft/bus'
 import {connect} from './redux'
 
-const ZWave = proxy('ZWave')
-export const zwaveApi = ({method, args}) => () => ZWave.dispatch({type: 'API', method, args})
+const
+    ZWave = proxy('ZWave'),
+    zwaveApi = ({method, args}) => ZWave.dispatch({type: 'API', method, args}),
+    Settings = proxy('Settings'),
+    settingsSet = state => Settings.setState(state)
 
 const
-    mapStateToProps = ({devices = {}}) => ({devices}),
-    mapDispatchToProps = dispatch => ({dispatchZWaveApi: action => dispatch(zwaveApi(action))})
+    mapState = ({settings}) => ({settings}),
+    mapDispatch = dispatch => ({
+        api: async (id, value) => {
+            await zwaveApi(id === 'add' && value ? {method: 'addNode', args: [true]}
+                : id === 'remove' && value ? {method: 'removeNode', args: []}
+                : {method: 'cancelControllerCommand', args: []}
+            )
+            await settingsSet({[`ZWave.${id}`]: value})
+        }
+    })
 
-export const AddDevice = connect(mapStateToProps, mapDispatchToProps)(class extends Component {
-    state = {add: false}
 
-    onChange = (add, _e) => {
-        this.props.dispatchZWaveApi(add ? {method: 'addNode', args: [true]} : {method: 'cancelControllerCommand', args: []})
-            .then(() => this.setState({add}))
+export const ServiceSettings = (ComposedComponent, props) => connect(mapState, mapDispatch)(class ServiceSettings extends Component {
+    componentWillUnmount () {
+        const {settings, api} = this.props
+        if (settings['ZWave.add']) api('add', false)
+        if (settings['ZWave.remove']) api('remove', false)
     }
 
-    render ({dispatchZWaveApi, devices}, {add}) {
+    onClick = e => {
+        const
+            {id} = e.currentTarget.dataset,
+            {settings, api} = this.props ,
+            value = settings[`ZWave.${id}`]
+        api(id, !value)
+    }
+
+    onChange = (value, e) => this.onClick(e)
+
+    render ({settings}) {
+        const
+            item = (label, value, id) =>
+                <ListItem label={label}>
+                    <Switch checked={value} data-id={id} onChange={this.onChange}/>
+                </ListItem>
         return (
-            <ListItem label="Add device">
-                <Switch checked={add} onChange={this.onChange}/>
-            </ListItem>
+            <ComposedComponent {...props}>
+                {item('Add device', settings['ZWave.add'], 'add')}
+                {item('Remove device', settings['ZWave.remove'], 'remove')}
+            </ComposedComponent>
         )
     }
 })
