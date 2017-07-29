5f4c101a612a3be3da2389d300bc8838890be081
Rob Huehn <rhuehn@gmail.com>
2017-07-29 08:58:43 -0400

    add service settings name prop for multiple zwave services

--- src/components/index.js
+++ src/components/index.js
@@ -4,42 +4,36 @@ import {proxy} from '@theatersoft/bus'
 import {connect} from './redux'
 
 const
-    ZWave = proxy('ZWave'),
-    zwaveApi = ({method, args}) => ZWave.dispatch({type: 'API', method, args}),
-    Settings = proxy('Settings'),
-    settingsSet = state => Settings.setState(state)
-
-const
     mapState = p => p,
     mapDispatch = dispatch => ({
-        api: async (id, value) => {
-            await zwaveApi(id === 'add' && value ? {method: 'addNode', args: [true]}
-                : id === 'remove' && value ? {method: 'removeNode', args: []}
-                : {method: 'cancelControllerCommand', args: []}
-            )
-            await settingsSet({[`ZWave.${id}`]: value})
+        api: async (name, id, value) => {
+            const
+                method = id === 'add' && value ? 'addNode' : id === 'remove' && value ? 'removeNode' : 'cancelControllerCommand',
+                args = [method === 'addNode']
+            await proxy(name).dispatch({type: 'API', method, args})
+            await proxy('Settings').setState({[`ZWave.${id}`]: value})
         }
     })
 
 
 export const ServiceSettings = (ComposedComponent, props) => connect(mapState, mapDispatch)(class ServiceSettings extends Component {
     componentWillUnmount () {
-        const {settings, api} = this.props
-        if (settings['ZWave.add']) api('add', false)
-        if (settings['ZWave.remove']) api('remove', false)
+        const {name = 'ZWave', settings, api} = this.props
+        if (settings['${name}.add']) api(name, 'add', false)
+        if (settings['${name}.remove']) api(name, 'remove', false)
     }
 
     onClick = e => {
         const
             {id} = e.currentTarget.dataset,
-            {settings, api} = this.props ,
-            value = settings[`ZWave.${id}`]
-        api(id, !value)
+            {name = 'ZWave', settings, api} = this.props ,
+            value = settings[`${name}.${id}`]
+        api(name, id, !value)
     }
 
     onChange = (value, e) => this.onClick(e)
 
-    render ({settings}) {
+    render ({name = 'ZWave', settings}) {
         const
             item = (label, value, id) =>
                 <ListItem label={label}>
@@ -47,8 +41,8 @@ export const ServiceSettings = (ComposedComponent, props) => connect(mapState, m
                 </ListItem>
         return (
             <ComposedComponent {...props}>
-                {item('Add device', settings['ZWave.add'], 'add')}
-                {item('Remove device', settings['ZWave.remove'], 'remove')}
+                {item('Add device', settings['${name}.add'], 'add')}
+                {item('Remove device', settings['${name}.remove'], 'remove')}
             </ComposedComponent>
         )
     }
