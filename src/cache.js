d914bb641eca9d6c6f2732dc02959450bb491980
Rob Huehn <rhuehn@gmail.com>
2017-08-24 08:27:15 -0700

    rename cache file to zwave.config.json

--- src/cache.js
+++ src/cache.js
@@ -4,7 +4,7 @@ import fs from 'fs'
 import {log} from './log'
 
 const THEATERSOFT_CONFIG_HOME = `${process.env.XDG_CONFIG_HOME || `${process.env.HOME}/.config`}/theatersoft`
-const filename = `${THEATERSOFT_CONFIG_HOME}/zwave-cache.json`
+const filename = `${THEATERSOFT_CONFIG_HOME}/zwave.config.json`
 const read = () => {try {return JSON.parse(fs.readFileSync(filename, 'utf8'))} catch (e) {}}
 const write = cache => fs.writeFileSync(filename, JSON.stringify(cache), 'utf8')
 let cache
@@ -13,7 +13,7 @@ import {deviceSet, nodeinfoSet} from './actions'
 
 export const load = dispatch => {
     cache = read() || {}
-    if (Object.keys(cache).length) log(`Loaded device cache: ${filename}`)
+    if (Object.keys(cache).length) log(`Loaded zwave device config: ${filename}`)
     Object.values(cache).forEach(({name, type, id, cid}) => {
         dispatch(deviceSet({name, type, id}))
         cid && dispatch(nodeinfoSet(Number(id), {cid}))
