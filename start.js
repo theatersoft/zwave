cb85b9b40466297b273a9e2d063b6b7b5a4b0b18
Rob Huehn <rhuehn@gmail.com>
2017-12-18 11:32:20 -0800

    convert to startLocalService

--- start.js
+++ start.js
@@ -1,24 +1,14 @@
-'use strict'
-const
-    bus = require('@theatersoft/bus').default,
-    options = {
-        module: '@theatersoft/zwave',
-        export: 'ZWave',
-        name: 'ZWave',
-        config: {
-            remotedev: 'localhost',
-            port: '/dev/zwave',
-            options: {
-                Logging: true,
-                ConsoleOutput: true,
-                "SaveLogLevel": 7
-            }
+require('@theatersoft/server/lib').startLocalService({
+    module: '@theatersoft/zwave',
+    export: 'ZWave',
+    name: 'ZWave',
+    config: {
+        remotedev: 'localhost',
+        port: '/dev/zwave',
+        options: {
+            Logging: true,
+            ConsoleOutput: true,
+            "SaveLogLevel": 7
         }
-    },
-    service = new (require(options.module)[options.export])()
-
-bus.start().then(() =>
-    service.start(options))
-
-process.on('SIGINT', () =>
-    service.stop().then(() => process.exit()))
+    }
+})
