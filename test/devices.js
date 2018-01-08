5398d2ed41a8d32d38117e716361ddfbba99610f
Rob Huehn <rhuehn@gmail.com>
2018-01-07 17:45:32 -0800

    update devices test for store changes

--- test/devices.js
+++ test/devices.js
@@ -14,9 +14,9 @@ bus.start().then(async () => {
 
         const all = Object.entries(nodes)
             .map(([nid, o]) => ({nid, ...o}))
-            .filter(({nid}) => nid != 1)
+            .filter(({nid, values}) => nid !== '1' && values)
 
-        log()
+        log(`\n${all.length} devices`)
         all.forEach(({nid, cid, type, manufacturer, product, values}) => {
             log(columnify(4, 16, 26, 14, 50)(nid, devices[nid].name, type, manufacturer, product))
         })
@@ -24,33 +24,32 @@ bus.start().then(async () => {
         const alarms = all.map(({values, ...o}) => ({
                 ...o,
                 values,
-                alarmValues: Object.values(values)
-                    .filter(({class_id}) => class_id === 113)
+                alarmValues: values[113] && Object.values(values[113])
                     .map(({label}) => label)
             }))
-            .filter(o => o.alarmValues.length)
+            .filter(o => o.alarmValues)
 
-        log()
+        log('\ncommand class alarm value labels')
         alarms
             .forEach(({nid, cid, type, manufacturer, product, alarmValues}) => {
                 log(columnify(4, 16, 16, 5, 26)(nid, devices[nid].name, devices[nid].type, cid, type, alarmValues.toString()))
             })
 
-        log()
+        log('\nalarm devices all command classes')
         alarms.forEach(o => {
             o.cids = {}
-            Object.values(o.values).forEach(v => o.cids[v.class_id] = true)
+            Object.keys(o.values).forEach(k => o.cids[k] = true)
             log(columnify(4, 16)(o.nid, devices[o.nid].name, Object.keys(o.cids).toString()))
         })
 
-        log()
+        log('\ncommand classes')
         const cids = alarms.reduce((cids, n) => (Object.keys(n.cids).forEach(cid => cids[cid] = true), cids), {})
         Object.keys(cids).forEach(cid => log(cid, inverseCC[cid]))
 
-        const matchCid = cid => all.filter(({values}) => Object.values(values).find(({class_id}) => class_id === cid))
+        const matchCid = cid => all.filter(({values}) => Object.keys(values).find(k => k == cid))
         const logCid = cid => log(`${cid}:`, matchCid(cid).map(({nid}) => nid).toString())
 
-        log()
+        log('\ndevices by selected command classes')
         logCid(48)
         logCid(49)
         logCid(113)
@@ -60,16 +59,15 @@ bus.start().then(async () => {
             matchCid(cid).forEach(({nid, values}) => log(`${nid}:`, getCidValuesValue(cid, values)))
         }
 
-        log()
+        log('\ncid values')
         logCidValues(113)
 
-        log()
+        log('\ncid values')
         logCidValues(48)
 
-        log('')
-        all.map(({nid, values}) => values[`${nid}-113-1-10`])
-            .filter(v => v)
-            .forEach(v => log(v.node_id, v.value))
+        log('\n113 Burglar values')
+        all.filter(({values}) => values[113] && values[113][10])
+            .forEach(({nid, values}) => log(nid, values[113][10].value))
 
     }
     catch (e) {console.log(e)}
