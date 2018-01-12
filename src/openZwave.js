63e57849eac0b3b39807855297b86ee00cbcc551
Rob Huehn <rhuehn@gmail.com>
2018-01-11 23:44:43 -0800

    tidy

--- src/openZwave.js
+++ src/openZwave.js
@@ -23,19 +23,40 @@ export function createZwave ({port: p, options}) {
 
 export function setZwaveStore (zwave, {dispatch}) {
     zwave
-        .on('connected', v => log('connected', v))
-        .on('driver ready', hid => log(`driver ready`, hid.toString(16)))
-        .on('driver failed', () => (log('driver failed'), zwave.disconnect(port)))
-        .on('node added', nid => dispatch(nodeSet(nid)))
-        .on('value added', (nid, cid, value) => dispatch(addValue(value)))
-        .on('value changed', (nid, cid, value) => dispatch(changeValue(value)))
-        .on('value removed', (nid, cid, index) => dispatch(valueRemoved(nid, cid, index)))
-        .on('node ready', (nid, nodeinfo) => dispatch(readyNode(nid, nodeinfo)))
-        .on('notification', (nid, notif) => log(`notification node ${nid} code ${codeKey[notif]}`))
+        .on('connected', v =>
+            log('connected', v)
+        )
+        .on('driver ready', hid =>
+            log(`driver ready`, hid.toString(16))
+        )
+        .on('driver failed', () => {
+            log('driver failed')
+            zwave.disconnect(port)
+        })
+        .on('node added', nid =>
+            dispatch(nodeSet(nid))
+        )
+        .on('value added', (nid, cid, value) =>
+            dispatch(addValue(value))
+        )
+        .on('value changed', (nid, cid, value) =>
+            dispatch(changeValue(value))
+        )
+        .on('value removed', (nid, cid, index) =>
+            dispatch(valueRemoved(nid, cid, index))
+        )
+        .on('node ready', (nid, nodeinfo) =>
+            dispatch(readyNode(nid, nodeinfo))
+        )
+        .on('notification', (nid, notif) =>
+            log(`notification node ${nid} code ${codeKey[notif]}`)
+        )
         .on('scan complete', () => {
             log('scan complete')
             zwave.writeConfig()
         })
-        .on('controller command', (r, s) => log('controller commmand feedback', r, s))
+        .on('controller command', (r, s) =>
+            log('controller commmand feedback', r, s)
+        )
 }
 
