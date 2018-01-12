5a0dfcdac9457bbd16f8c9679732d37274ff92a5
Rob Huehn <rhuehn@gmail.com>
2018-01-11 23:48:51 -0800

    normalize basic value

--- src/actions/valueActions.js
+++ src/actions/valueActions.js
@@ -24,7 +24,7 @@ const
         [CommandClass.Basic]: // 32
             ({value}) => ({
                 $merge: {
-                    basic: {value} // 0 | 255
+                    basic: {value: Boolean(value)} // 0 | 255
                 }
             }),
         [CommandClass.BinarySensor]: // 48
