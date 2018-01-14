691da5c396c923cd0dfbc266c44119008ea03a26
Rob Huehn <rhuehn@gmail.com>
2018-01-14 12:30:45 -0800

    also map device values wip

--- src/actions/valueActions.js
+++ src/actions/valueActions.js
@@ -13,43 +13,41 @@ const maps = {
         [CommandClass.Alarm]: // 113
             ({value, index}) => {
                 const key = {0: 'type', 1: 'level', 10: 'burglar'}[index]
-                return key && {alarm: {$auto: {$merge: {[key]: value}}}}
+                return [key && {alarm: {$auto: {$merge: {[key]: value}}}}]
             },
         [CommandClass.Basic]: // 32
-            ({value}) => ({
+            ({value}) => ([{
                 $merge: {
                     basic: {value: Boolean(value)} // 0 | 255
                 }
-            }),
+            }]),
         [CommandClass.BinarySensor]: // 48
-            ({value}) => ({
+            ({value}) => ([{
                 $merge: {
                     binarySensor: {value} // boolean
                 }
-            }),
+            }]),
         [CommandClass.Battery]: // 128
-            ({value}) => ({
+            ({value}) => ([{
                 $merge: {
                     battery: {value} // 0-100 %
                 }
-            }),
+            }]),
         [CommandClass.WakeUp]: // 132
             ({value, index}) => {
                 const key = {0: 'value', 1: 'min', 2: 'max', 4: 'step'}[index]
-                return key && {wake: {$auto: {$merge: {[key]: value}}}}
+                return [key && {wake: {$auto: {$merge: {[key]: value}}}}]
             }
     },
-    dispatchZwaveValueSet = (dispatch, nid, cid, value) => {
-        const val = maps[cid] && maps[cid](value)
-        if (val) dispatch(zwaveValueSet(nid, val))
-    }
+    mapValue = (cid, value) => maps[cid] ? maps[cid](value) : []
 
 export const
     addValue = value => dispatch => {
         dispatch(valueSet(value))
         const {class_id: cid, node_id: nid} = value
         if (nid === 1) return
-        dispatchZwaveValueSet(dispatch, nid, cid, value)
+        const [zval] = mapValue(cid, value)
+        if (zval) dispatch(zwaveValueSet(nid, zval))
     },
     changeValue = _value => (dispatch, getState) => {
         const
@@ -58,9 +56,16 @@ export const
             node = state.nodes[nid]
         if (node.values[cid][vid].value === value.value) return
         dispatch(valueSet(_value)) //TODO
+
         const device = state.devices[nid]
         if (!device) return
-        dispatchZwaveValueSet(dispatch, nid, cid, value)
+
+        const [zval, dval] = mapValue(cid, value)
+        if (zval) dispatch(zwaveValueSet(nid, zval))
+
+        if (dval && dval.value !== device.value)
+            dispatch(timestampMotion(deviceValueSet(nid, dval), Interface.SENSOR_BINARY))
+
         const
             intf = interfaceOfType(device.type),
             _cid = node.cid || cidOfInterface(intf), /// ???
