aa00072157a56d2b7f1114109ed74faa30d1697d
Rob Huehn <rhuehn@gmail.com>
2018-01-14 14:40:59 -0800

    mv utils to node actions

--- src/actions/nodeActions.js
+++ src/actions/nodeActions.js
@@ -1,7 +1,63 @@
-import {Interface, interfaceOfType, switchActions, dimmerActions} from '@theatersoft/device'
 import {log} from '../log'
 import {nodeinfoSet, deviceSet} from './index'
-import {updateNodeDevice} from '../utils'
+import CommandClass from "../CommandClass"
+import {Type, Interface, interfaceOfType} from '@theatersoft/device'
+import {update} from '../cache'
+
+const
+    cidMap = ({manufacturerid, producttype, productid}) => ({
+        "014a00010002": CommandClass.BinarySensor, // Ecolink Door/Window Sensor http://products.z-wavealliance.org/products/1498
+        "025800030083": CommandClass.BinarySensor // Coolcam NAS-PD01ZE Motion Sensor (PIR) http://products.z-wavealliance.org/ProductManual/File?folder=&filename=Manuals/1920/Motion%20Detector%20User%20Guide%20EU%20V3.3.pdf
+    }[`${manufacturerid.slice(2)}${producttype.slice(2)}${productid.slice(2)}`]),
+    cidOfInterface = intf => ({
+        [Interface.SWITCH_BINARY]: CommandClass.BinarySwitch,
+        [Interface.SWITCH_MULTILEVEL]: CommandClass.MultilevelSwitch,
+        [Interface.SENSOR_BINARY]: CommandClass.Alarm
+    }[intf]),
+    getCidValueIndex = cid =>
+        cid === CommandClass.Alarm ? 1 : 0,
+    getCidValuesValue = (cid, values) => {
+        const value = Object.values(values[cid] || {})
+            .find(v => v.index === getCidValueIndex(cid))
+        return value && value.value
+    },
+    normalizeInterfaceValue = (intf, value) => {
+        switch (intf) {
+        case Interface.SWITCH_BINARY:
+        case Interface.SENSOR_BINARY:
+            return !!value
+        }
+        return value
+    },
+    typeOfValues = values => {
+        const map = new Map([
+            [CommandClass.BinarySwitch, Type.Switch],
+            [CommandClass.MultilevelSwitch, Type.Dimmer],
+            [CommandClass.Alarm, Type.OpenSensor]
+        ])
+        for (const [cid, type] of map.entries())
+            if (getCidValuesValue(cid, values) !== undefined) return type
+    },
+    updateNodeDevice = (nid, nodeinfo) => {
+        const {product, values} = nodeinfo
+        let device,
+            {name} = nodeinfo,
+            type = typeOfValues(values)
+        if (type === Type.OpenSensor && product && product.includes('Motion'))
+            type = Type.MotionSensor
+        if (type) {
+            const
+                id = String(nid),
+                intf = interfaceOfType(type)
+            let cid = cidMap(nodeinfo) || cidOfInterface(intf);
+            ({name, type, cid} = update({id, name, type, cid}))
+            if (!name) name = `ZWave.${id}`
+            const value = normalizeInterfaceValue(intf, getCidValuesValue(cid, values))
+            device = {name, value, type, id}
+            nodeinfo = {...nodeinfo, cid}
+        }
+        return [device, nodeinfo]
+    }
 
 export const
     readyNode = (nid, nodeinfo) => (dispatch, getState, {zwave}) => {
--- src/utils.js
+++ src/utils.js
@@ -1,78 +1,14 @@
-import CommandClass from './CommandClass'
-import {Type, Interface, interfaceOfType} from '@theatersoft/device'
-// import {log} from './log'
-import {update} from './cache'
-
 export const
     vidOfIndexInstance = (index, instance = 1) =>
         instance === 1 ? `${index}` : `${index}-${instance}`,
     fromOzwValue = ({value_id: _vid, node_id: nid, class_id: cid, ...rest}) =>
         ([nid, vidOfIndexInstance(rest.index, rest.instance), cid, rest]),
-    toOzwValue = ({nid: node_id, cid: class_id, ...rest}) =>
-        ({node_id, class_id, ...rest}),
-    updateNodeDevice = (nid, nodeinfo) => {
-        const {product, values} = nodeinfo
-        let device,
-            {name} = nodeinfo,
-            type = typeOfValues(values)
-        if (type === Type.OpenSensor && product && product.includes('Motion'))
-            type = Type.MotionSensor
-        if (type) {
-            const
-                id = String(nid),
-                intf = interfaceOfType(type)
-            let cid = cidMap(nodeinfo) || cidOfInterface(intf);
-            ({name, type, cid} = update({id, name, type, cid}))
-            if (!name) name = `ZWave.${id}`
-            const value = normalizeInterfaceValue(intf, getCidValuesValue(cid, values))
-            device = {name, value, type, id}
-            nodeinfo = {...nodeinfo, cid}
-        }
-        return [device, nodeinfo]
-    },
-    typeOfValues = values => {
-        const map = new Map([
-            [CommandClass.BinarySwitch, Type.Switch],
-            [CommandClass.MultilevelSwitch, Type.Dimmer],
-            [CommandClass.Alarm, Type.OpenSensor]
-        ])
-        for (const [cid, type] of map.entries())
-            if (getCidValuesValue(cid, values) !== undefined) return type
-    },
-    cidOfInterface = intf => ({
-        [Interface.SWITCH_BINARY]: CommandClass.BinarySwitch,
-        [Interface.SWITCH_MULTILEVEL]: CommandClass.MultilevelSwitch,
-        [Interface.SENSOR_BINARY]: CommandClass.Alarm
-    }[intf]),
-    getNodeValue = (nid, nodes) => {
-        const {cid, values} = nodes[nid]
-        return values[cid][vidOfIndexInstance(getCidValueIndex(cid))]
-    },
     getNodeOzwValue = (nid, nodes) => {
         const
             {cid} = nodes[nid],
             {index, instance = 1} = getCidValueIndex(cid)
         return {node_id: nid, class_id: cid, index, instance}
-    },
-    getCidValuesValue = (cid, values) => {
-        const value = Object.values(values[cid] || {})
-            .find(v => v.index === getCidValueIndex(cid))
-        return value && value.value
-    },
-    normalizeInterfaceValue = (intf, value) => {
-        switch (intf) {
-        case Interface.SWITCH_BINARY:
-        case Interface.SENSOR_BINARY:
-            return !!value
-        }
-        return value
-    },
-    getCidValueIndex = cid =>
-        cid === CommandClass.Alarm ? 1 : 0,
-    cidMap = ({manufacturerid, producttype, productid}) => ({
-        "014a00010002": CommandClass.BinarySensor, // Ecolink Door/Window Sensor http://products.z-wavealliance.org/products/1498
-        "025800030083": CommandClass.BinarySensor // Coolcam NAS-PD01ZE Motion Sensor (PIR) http://products.z-wavealliance.org/ProductManual/File?folder=&filename=Manuals/1920/Motion%20Detector%20User%20Guide%20EU%20V3.3.pdf
-    }[`${manufacturerid.slice(2)}${producttype.slice(2)}${productid.slice(2)}`])
+    }
 
 import fs from 'fs'
 
