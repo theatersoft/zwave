c3c03b6442c5caa0379f8cfecaf8fdcc2980c752
Rob Huehn <rhuehn@gmail.com>
2018-01-19 21:17:29 -0800

    use service name only and get config from config.json

--- start.js
+++ start.js
@@ -1,14 +1,1 @@
-require('@theatersoft/server/lib').startLocalService({
-    module: '@theatersoft/zwave',
-    export: 'ZWave',
-    name: process.argv[3] || 'ZWave',
-    config: {
-        remotedev: 'localhost',
-        port: process.argv[2] || '/dev/zwave',
-        options: {
-            Logging: true,
-            ConsoleOutput: true,
-            SaveLogLevel: 7
-        }
-    }
-})
+require('@theatersoft/server/lib').startLocalService(process.argv[3])
